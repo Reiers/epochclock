0f8eb83cf603e976843fe3a628eade32f0828876
Nicklas <63351350+Reiers@users.noreply.github.com>
2023-03-19 18:02:32 +0100

    Update epochclock.js

--- epochclock.js
+++ epochclock.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   function epochToDate(epoch) {
-    const filecoinGenesisTimestamp = 1598313600; // UNIX timestamp of the Filecoin genesis block (2020-08-25 00:00:00 UTC)
+    const filecoinGenesisTimestamp = 1598306400; // UNIX timestamp of the Filecoin genesis block (2020-08-24 22:00:00 UTC)
     const epochDuration = 30 * 1000; // 30 seconds per epoch
     const epochTimestamp = new Date((filecoinGenesisTimestamp * 1000) + epoch * epochDuration);
     return epochTimestamp;
